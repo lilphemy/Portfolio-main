80204f294092922bf7acdf636b49252b7a1764ff
Obafemi Oladapo <lilphemy@gmail.com>
2024-10-04 12:21:31 +0100

    minor TS updates on source code files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,11 @@
 
 import React, {useState} from "react"
 import {HeaderComp, SmallScreenNav} from "../components/Headnav/headnav"
-import IntroBlock from "../components/introcomp/introcomp"
+import IntroBlock from "../components/IntroComp/introcomp"
 import AboutComp from "../components/aboutme/abooutme"
 import SkillComp from "../components/skillsfolder/skillcomp"
 import ProjectDisplay from "../components/projectscomp/projectcomp"
-import ContactForm from "../components/contactform/contactform"
+//import ContactForm from "../components/contactform/contactform"
 import FooterCase from "../components/footerfile/footer"
 
 
